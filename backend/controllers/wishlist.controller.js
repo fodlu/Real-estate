cfe8146aaa8295f6963f78ac1e38b7a479a3cb06
Fadilulahi Opeyemi Musediq <musediqopeyemi@gmail.com>
2026-07-04 15:09:47 -0400

    repository initialized

---
+++ backend/controllers/wishlist.controller.js
@@ -0,0 +1,79 @@
+import WishlistModel from "../models/wishlist.model.js";
+
+// to add property to wishlist
+export const addWishlist = async (req, res) => {
+	try {
+		const propertyId = req.params.propertyId;
+
+		const existing = await WishlistModel.findOne({
+			user: req.user._id,
+			property: propertyId,
+		});
+
+		if (existing) {
+			res.status(200).json({
+				success: true,
+				message: "Already in the wishlist",
+			});
+		}
+
+		await WishlistModel.create({
+			user: req.user._id,
+			property: propertyId,
+		});
+
+		res.status(201).json({
+			success: true,
+			message: "Added to the wishlist",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+
+// to get the property that is on the wishlist
+export const getWishList = async (req, res) => {
+    try {
+        const data = await WishlistModel.find({
+            user: req.user._id
+        }).populate("property");
+
+        res.status(200).json(data)
+    } catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+}
+
+// to remove a property from wishlist
+export const removeWishlist = async (req, res) => {
+    try {
+        const propertyId = req.params.id;
+        const result = await WishlistModel.findOneAndDelete({
+            user: req.user._id,
+			property: propertyId,
+        })
+
+        if(!result) {
+            return res.status(404).json({
+                success: false,
+                message: "Wishlist not found"
+            })
+        }
+
+        res.status(201).json({
+            success: true,
+            message: "Removed from wishlist"
+        })
+    } catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+}
---
+++ backend/routes/chat.routes.js
@@ -0,0 +1,160 @@
+import express from "express";
+import ChatModel from "../models/chat.model.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+ChatRouter.use(protect);
+
+// to create a chat
+ChatRouter.post("/start", async (req, res) => {
+	try {
+		const { propertyId, sellerId, buyerId: providedBuyerId } = req.body;
+		let buyerId, finalSellerId;
+		if (req.user.role === "seller") {
+			buyerId = providedBuyerId;
+			finalSellerId = req.user._id;
+		} else {
+			buyerId = req.user._id;
+			finalSellerId = sellerId;
+		}
+
+		if (!buyerId || !finalSellerId) {
+			return res.status(400).json({
+				message: "Missing buyer or the seller id",
+			});
+		}
+
+		// check for an existing chat between the buyer and the seller
+		let chat = await ChatModel.findOne({
+			buyer: buyerId,
+			seller: finalSellerId,
+		});
+
+		if (!chat) {
+			chat = await ChatModel.create({
+				property: propertyId, // initial property context
+				buyer: buyerId,
+				seller: finalSellerId,
+				messages: [],
+			});
+		}
+
+		chat = await ChatModel.findById(chat._id)
+			.populate("buyer", "name email profilePic")
+			.populate("seller", "name email profilePic")
+			.populate("property", "title price images");
+
+		res.json(chat);
+	} catch (error) {
+		res.status(500).json({
+			message: "Error creating chat or getting previous ones",
+			error: error.message,
+		});
+	}
+});
+
+// to send message
+ChatRouter.post("/send", async (req, res) => {
+	try {
+		const { chatId, text, image } = req.body;
+		const userId = req.user.id;
+
+		const chat = await ChatModel.findById(chatId);
+		if (!chat)
+			return res.status(404).json({
+				message: "Chat not found",
+			});
+
+		// ensure sender is part of the chat
+		if (chat.buyer.toString() !== userId && chat.seller.toString() !== userId) {
+			return res.status(403).json({
+				message: "You are not authorized to send message in this chat",
+			});
+		}
+
+		const newMessage = {
+			sender: userId,
+			text,
+			image,
+			createdAt: new Date(),
+		};
+		chat.messages.push(newMessage);
+		await chat.save();
+
+		const savedMessage = chat.messages[chat.message.length - 1];
+		res.json({ chat, newMessage: savedMessage });
+	} catch (error) {
+		res.status(500).json({
+			message: "Error sending the message",
+			error: error.message,
+		});
+	}
+});
+
+// to get chats for user
+ChatRouter.get("/user", async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const chat = await ChatModel.find({
+			$or: [{ buyer: userId }, { seller: sellerId }],
+		})
+			.populate("buyer", "name email profilePic")
+			.populate("seller", "name email profilePic")
+			.populate("property", "title price images")
+			.sort({ updatedAt: -1 });
+
+		res.json(chats);
+	} catch (error) {
+		res.status(500).json({
+			message: "Error fetching the user chats",
+			error: error.message,
+		});
+	}
+});
+
+// to get chat message
+ChatRouter.get('/:chatId', async (req, res ) => {
+    try {
+        const chat = await ChatModel.findById(req.params.chatId).populate('messages.sender', 'name profilePic')
+
+        if(!chat) return res.status(404).json({message: "Chat not found"});
+
+        const userId = req.user._id.toString();
+        if(chat.buyer.toString() !== userId && chat.seller.toString() !== userId) {
+            return res.status(403).json({
+                message: "You are not authorized"
+            });
+        }
+
+        res.json(chat)
+    } catch (error) {
+		res.status(500).json({
+			message: "Error fetching the chat messages",
+			error: error.message,
+		});
+	}
+});
+
+// to delete an entire chat
+ChatRouter.delete('/:chatId', async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const chat = await ChatModel.findById(req.params.chatId)
+
+        if(!chat) return res.status(404).json({message: "Chat not found"});
+
+        // to ensure the user is part of the chat
+        if(chat.buyer.toString() !== userId.toString() && chat.seller.toString() !== userId.toString()) {
+            return res.status(403).json({
+                message: "You are not authorized"
+            });
+        }
+
+        await ChatModel.findByIdAndDelete(req.params.chatId);
+        res.json({message : "Chat deleted successfully!"})
+    } catch (error) {
+		res.status(500).json({
+			message: "Error fetching the chat messages",
+			error: error.message,
+		});
+	}
+});
